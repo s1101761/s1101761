a7d1cffcf801bf863686860b111244a51337ea45
s1101761 <s1101761@gm.pu.edu.tw>
2023-05-31 10:15:33 +0800

    v.2

--- app/src/main/java/com/example/s1101761/MainActivity.kt
+++ app/src/main/java/com/example/s1101761/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.s1101761
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.s1101761.ui.theme.S1101761Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,7 @@ class MainActivity : ComponentActivity() {
             S1101761Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("顏偉森")
                 }
             }
         }
@@ -28,7 +37,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+
+        Text(text = "作者:資管二B $name!")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Image(
+                    painter = painterResource(id = R.drawable.map),
+            contentDescription = "手掌圖片",
+            )
+        }
+
+
+    }
 }
 
 @Preview(showBackground = true)
