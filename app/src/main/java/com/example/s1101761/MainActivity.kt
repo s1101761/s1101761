3f94c06b16f73aaf3a7a8371c687162e899a1be7
s1101761 <s1101761@gm.pu.edu.tw>
2023-05-31 11:33:57 +0800

    v.3

--- app/src/main/java/com/example/s1101761/MainActivity.kt
+++ app/src/main/java/com/example/s1101761/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.s1101761
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -14,9 +15,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -35,21 +46,35 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Greeting(name: String) {
     function()
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Canvas(modifier = Modifier){
+            drawRect(Color.Blue,  Offset(790f, 170f), Size(30f,30f))
+            drawRect(Color.Blue,  Offset(1600f, 870f), Size(30f,30f))
+        }
+    }
+
 }
 @Composable
 fun function() {
+
+
+
     Text(text = "作者:資管二B 顏偉森" )
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.map),
-            contentDescription = "手掌圖片",
-        )
+    Column{
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.map),
+                contentDescription = "手掌圖片",)
+        }
     }
 }
 
